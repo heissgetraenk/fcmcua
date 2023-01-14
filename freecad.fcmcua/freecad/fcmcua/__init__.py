2d2c9b780134796fbe67fde14ac1e69d5095f6bf
heissgetraenk <unreal.steve.matthews@gmail.com>
2023-01-14 15:07:14 +0100

    added dependencies to repo to simplify install

--- freecad.fcmcua/freecad/fcmcua/__init__.py
+++ freecad.fcmcua/freecad/fcmcua/__init__.py
@@ -1,22 +1,14 @@
 import os
+import sys
 from .version import __version__
 
 ICONPATH = os.path.join(os.path.dirname(__file__), "resources")
 
-#set system paths, so that both the workbench files and the libraries it needs are found by FreeCAD
-import sys
+# read number of axes, actuators from file
 with open(os.path.join(os.path.dirname(__file__), "fcmcua.ini")) as settings:
     strng = settings.read()
-    py_path = wb_path = ""
     __axes__ = __actuators__ = 0
     for s in strng.split():
-        # path to python libraries
-        if py_path == "":
-            py_path = (s[len("PyPath="):] if (s[:len("PyPath=")] == "PyPath=") else "" )
-        # path to the workbench files
-        if wb_path == "":
-            wb_path = (s[len("WbPath="):] if (s[:len("WbPath=")] == "WbPath=") else "" )
-        # configuration: number of axes and actuators
         if __axes__ == 0:
             __axes__ = int((s[len("AxNum="):] if (s[:len("AxNum=")] == "AxNum=") else "0" ))
         if __actuators__ == 0:
@@ -25,9 +17,13 @@ with open(os.path.join(os.path.dirname(__file__), "fcmcua.ini")) as settings:
 AXES = __axes__
 ACTUATORS = __actuators__
 
-#add paths to sys.path so that FreeCAD's python interpreter finds all files
+# add paths to sys.path so that FreeCAD's python interpreter finds all files
+py_path = os.path.join(os.path.dirname(__file__), "dependencies")
+
 if os.path.exists(os.path.dirname(py_path)):
     sys.path.append(py_path)
+
+wb_path = os.path.dirname(__file__)
 
 if os.path.exists(os.path.dirname(wb_path)):
     sys.path.append(wb_path)
